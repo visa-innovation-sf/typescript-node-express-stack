c6e810d7978950933b8ff74de61ee9a0944eb107
Wee <kewee@visa.com>
2023-05-16 22:58:51 +0800

    Add environment variables

---
+++ src/configurations/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+interface Config {
+    port: number,
+    whitelist: string[]
+}
+
+const env = process.env.NODE_ENV ?? '';
+
+dotenv.config({
+	path: path.resolve(__dirname, env ? `.env.${env}` : '.env'),
+});
+
+const config: Config = {
+	port: parseInt(process.env.PORT ?? ''),
+	whitelist: [process.env.WHITELIST ?? ''],
+};
+
+export default config;
