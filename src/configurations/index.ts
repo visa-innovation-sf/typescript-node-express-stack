5edef1dedd55cb7fa6c4c82e16c1b0e1e2134d99
Wee <kewee@visa.com>
2023-05-23 09:36:33 +0800

    File linting

--- src/configurations/index.ts
+++ src/configurations/index.ts
@@ -2,33 +2,33 @@ import dotenv from 'dotenv';
 import path from 'path';
 
 interface Config {
-	aws: {
-		credentials: {
-			accessKeyId: string;
-			secretAccessKey: string;
-		};
-		region?: string;
-	};
-    port: number,
-    whitelist: string[];
+  aws: {
+    credentials: {
+      accessKeyId: string;
+      secretAccessKey: string;
+    };
+    region?: string;
+  };
+  port: number;
+  whitelist: string[];
 }
 
 const env = process.env.NODE_ENV ?? '';
 
 dotenv.config({
-	path: path.resolve(__dirname, env ? `.env.${env}` : '.env'),
+  path: path.resolve(__dirname, env ? `.env.${env}` : '.env'),
 });
 
 const config: Config = {
-	aws: {
-		credentials: {
-			accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '',
-			secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
-		},
-		region: process.env.AWS_REGION,
-	},
-	port: parseInt(process.env.PORT ?? ''),
-	whitelist: process.env.WHITELIST ? process.env.WHITELIST.split(',') : [],
+  aws: {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID ?? '.env',
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+    },
+    region: process.env.AWS_REGION,
+  },
+  port: parseInt(process.env.PORT ?? ''),
+  whitelist: process.env.WHITELIST ? process.env.WHITELIST.split(',') : [],
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,12 @@ const app = express();
 
 app.use(helmet());
 app.use(cors({ origin: config.whitelist }));
- 
+
 app.get('/', (_req, res) => {
 	res.send('Welcome to VCXC POC Backend Service');
 });
 app.use('/', router);
 
-app.use(
-	'/docs',
-	swaggerUi.serve,
-	swaggerUi.setup(yaml.load('./api/index.yaml')),
-);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(yaml.load('./api/index.yaml')));
 
 app.listen(config.port);
